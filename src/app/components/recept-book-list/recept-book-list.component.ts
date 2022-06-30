d9befb42effd22a527e0dec59d35c6381c51b285
Roman D <96253839+romka113@users.noreply.github.com>
2022-06-30 18:02:13 +0300

    Filtravimas

--- src/app/components/recept-book-list/recept-book-list.component.ts
+++ src/app/components/recept-book-list/recept-book-list.component.ts
@@ -9,18 +9,42 @@ import { ReceptService } from '../../services/recept.service';
 })
 export class ReceptBookListComponent implements OnInit {
   public receptBook: Recept[] = [];
+  public receptBookDisplay: Recept[] = [];
+  public filterPusryciai: boolean = true;
+  public filterPriespieciai: boolean = true;
+  public filterPietus: boolean = true;
+  public filterVakariene: boolean = true;
   constructor(private receptServ: ReceptService) {}
   private geRecept() {
     this.receptServ.getrecept().subscribe((response) => {
       this.receptBook = response;
+      this.displayRecept();
     });
   }
   ngOnInit(): void {
     this.geRecept();
+
     this.receptServ.likesCount.subscribe(() => {
       this.geRecept();
     });
     this.receptServ.mealCount();
+  }
+  public displayRecept() {
+    this.receptBookDisplay = [];
+    this.receptBook.forEach((r) => {
+      if (this.filterPusryciai && r.eatingTime == 'Pusryciai') {
+        this.receptBookDisplay.push(r);
+      }
+      if (this.filterPriespieciai && r.eatingTime == 'Priespieciai') {
+        this.receptBookDisplay.push(r);
+      }
+      if (this.filterPietus && r.eatingTime == 'Pietus') {
+        this.receptBookDisplay.push(r);
+      }
+      if (this.filterVakariene && r.eatingTime == 'Vakriene') {
+        this.receptBookDisplay.push(r);
+      }
+    });
   }
   public increaseLikesCount(id?: string) {
     if (id != null) {
